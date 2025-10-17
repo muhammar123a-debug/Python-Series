de76adc43321122a1e8d3f68b8f530fc77c163a9
muhammar123a-debug <muhammar123a@gmail.com>
2025-10-17 11:27:17 +0500

    add code

--- functions.py
+++ functions.py
@@ -9,3 +9,18 @@ def myFunction(name):
 myFunction("Hello")
 myFunction("How are you?")
 myFunction("Goodbye")
+
+def functionArgs(*argd):
+    print("type of agruments:", type(argd))
+    print("Number 0 agruments:", argd[0])
+    print("Number 2 agruments:", argd[2])
+    print("ALL agruments:", argd)
+
+# functionArgs(1, 2, 3, 4, 5)
+functionArgs("apple", "banana", "cherry")
+
+def my_func():
+    x = 300
+    print(x)
+
+my_func()
